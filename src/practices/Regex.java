59556d8f0ba40e76e6a1d408972c3ee472d8339f
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-10-03 22:41:43 +0600

    regular expression

---
+++ src/practices/Regex.java
@@ -0,0 +1,15 @@
+package practices;
+import java.util.Scanner;
+public class Regex {
+   public static void main(String[] args) {
+	  System.out.println(validate("ABCDEFabcdef123450"));
+		System.out.println(validate("SQL"));		
+		System.out.println(validate("Java"));
+		System.out.println(validate("*&%@#!}{"));
+		System.out.println(validate("w3resource.com"));
+    }
+
+   public static boolean validate(String text) {
+    return text.matches("^[\\w]+$");
+  }
+}
