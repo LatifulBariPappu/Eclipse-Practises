f3511fdd1a0ccde23f3677f4e956af54614e7f62
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-10-10 20:15:05 +0600

    the current time

--- src/practices/DateTime.java
+++ src/practices/DateTime.java
@@ -1,9 +1,12 @@
 package practices;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class DateTime {
   public static void main(String[] args) {
     LocalDate myObj = LocalDate.now();
     System.out.println("the current date : "+myObj);
+    LocalTime myObj1 = LocalTime.now();
+    System.out.println("the current time : "+myObj1);
   }
 }
