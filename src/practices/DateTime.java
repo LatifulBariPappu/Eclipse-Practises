b13c2a1cb334caa985bc14538df11f53c4988e0b
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-10-10 20:18:27 +0600

    Local Date and Time together

--- src/practices/DateTime.java
+++ src/practices/DateTime.java
@@ -1,6 +1,7 @@
 package practices;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public class DateTime {
   public static void main(String[] args) {
@@ -8,5 +9,7 @@ public class DateTime {
     System.out.println("the current date : "+myObj);
     LocalTime myObj1 = LocalTime.now();
     System.out.println("the current time : "+myObj1);
+    LocalDateTime myDateTimeObj = LocalDateTime.now();
+    System.out.println("Before formatting date and time: " + myDateTimeObj);
   }
 }
