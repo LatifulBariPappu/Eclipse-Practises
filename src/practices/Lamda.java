2936c2ac086633223c9a2df96d14b5f262f85cd1
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-10-14 09:40:19 +0600

    lamda expression

---
+++ src/practices/Lamda.java
@@ -0,0 +1,13 @@
+package practices;
+import java.util.ArrayList;
+
+public class Lamda {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(5);
+    numbers.add(9);
+    numbers.add(8);
+    numbers.add(1);
+    numbers.forEach( (n) -> { System.out.println(n); } ); //ArrayList's for each method
+  }
+}
