70b399fead59261b6afc27ec822364858534310d
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-10-15 19:14:31 +0600

    lamba as a parameter

--- src/practices/Lamda.java
+++ src/practices/Lamda.java
@@ -1,6 +1,9 @@
 package practices;
 import java.util.ArrayList;
 
+interface StringFunction {
+  String run(String str);
+}
 public class Lamda {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<Integer>();
@@ -11,5 +14,15 @@ public class Lamda {
     numbers.forEach( (n) -> { System.out.println(n); } ); //ArrayList's for each method
     Consumer<Integer> method = (n) -> { System.out.println(n);}; //Consumer interface
     numbers.forEach( method );
+
+    StringFunction exclaim = (s) -> s + "!";
+    StringFunction ask = (s) -> s + "?";
+    printFormatted("Hello", exclaim);
+    printFormatted("Hello", ask);
+    
+    public static void printFormatted(String str, StringFunction format) {
+    String result = format.run(str);
+    System.out.println(result);
+  }
   }
 }
