a89fffd60c4ec1815df24ae25831c757f9966112
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-10-18 14:07:46 +0600

    count the word

---
+++ src/practices/CountWord.java
@@ -0,0 +1,8 @@
+package practices;
+public class CountWord{
+  public static void main(String[] args){
+    String words = "One Two Three Four";
+    int countWords = words.split("\\s").length;
+    System.out.println(countWords);
+  }
+}
