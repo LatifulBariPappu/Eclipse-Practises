28bd26e8b5b95687f040d5775740ad0dcc383619
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-10-20 11:55:05 +0600

    LinkedList in java

---
+++ src/practices/LinkedListJava.java
@@ -0,0 +1,22 @@
+package practices;
+import java.util.LinkedList;
+public class LinkedListJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		    LinkedList<String> cars = new LinkedList<String>();
+		    cars.add("Volvo");
+		    cars.add("BMW");
+		    cars.add("Ford");
+		    cars.add("Mazda");
+		    System.out.println(cars);
+		    cars.addFirst("Toyota");
+		    cars.addLast("Benz");
+		    System.out.println(cars);
+		    cars.removeLast();
+		    System.out.println(cars);
+		    System.out.println(cars.getLast());
+		    
+		  }
+	}
