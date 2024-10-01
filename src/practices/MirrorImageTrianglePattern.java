a57c1351569d5b65284b65d34bce5a20a45ed59b
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2024-10-01 21:44:17 +0600

    mirror image triangle pattern using java

---
+++ src/practices/MirrorImageTrianglePattern.java
@@ -0,0 +1,29 @@
+
+public class MirrorImageTrianglePattern {
+	
+	public static void main(String[] args) {
+		
+		int i,j,n=5;
+		
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print(" ");
+			}
+			for(j=i;j<=n;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for(i=1;i<=n;i++) {
+			for(j=i;j<=n;j++) {
+				System.out.print(" ");
+			}
+			for(j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
